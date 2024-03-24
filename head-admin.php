7903f4a54dd4bdaa16eea559f5ec35aceb348a14
Omina Abu-elhassan <130615954+omniaabuelhassan@users.noreply.github.com>
2024-03-24 02:33:27 +0200

    upload  admin header page

---
+++ head-admin.php
@@ -0,0 +1,20 @@
+<header class="header">
+
+   <div class="flex">
+
+      <a href="#" class="logo">Islamic_Clothes_shop</a>
+
+      <nav class="navbar">
+         <a href="admin.php">add products</a>
+         <a href="clothes.php">view products</a>
+      </nav>
+
+      
+
+      <a href="shopping_cart.php" class="cart">cart <span> </a>
+
+      <div id="menu-btn" class="fas fa-bars"></div>
+
+   </div>
+
+</header>
